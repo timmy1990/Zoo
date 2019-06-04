1238ea18de6b764b3a36159e21dd9909dbf94a3c
timmy1990 <turner.jeremy@gmx.de>
2019-06-04 16:42:51 +0200

    ZooMap

---
+++ Testing/hmTest/Assets/gyroTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gyroTest : MonoBehaviour
+{
+    GameObject gyroView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gyroView = new GameObject("gyroTest");
+        gyroView.transform.position = this.transform.position;
+        this.transform.parent = gyroView.transform;
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gyroView.transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+        this.transform.Rotate(-Input.gyro.rotationRateUnbiased.x, 0, 0);
+    }
+}
