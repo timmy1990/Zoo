9842d6c5220b7d0912cb6d9cccd5775653266ecc
timmy1990 <turner.jeremy@gmx.de>
2019-07-05 09:27:30 +0200

    HandsOnDemo

--- MASTER/ZooMstr/ZooMaster/Assets/Scripts/playerCam.cs
+++ MASTER/ZooMstr/ZooMaster/Assets/Scripts/playerCam.cs
@@ -8,6 +8,30 @@ public class playerCam : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
+    private Vector2 touchPos;
+    private float swipeResistance;
+
+   private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(0))
+        {
+            touchPos = Input.mousePosition;
+
+        }
+        if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(0))
+        {
+            float swipeForce = touchPos.x - Input.mousePosition.x;
+            if (Mathf.Abs(swipeForce) > swipeResistance)
+            {
+                if (swipeForce < 0)
+                    SlideCamera(true);
+                else
+                    SlideCamera(false);
+            }
+        }
+    }
+
      void FixedUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
@@ -15,8 +39,19 @@ public class playerCam : MonoBehaviour
 
         transform.position = smoothedPosition;
 
-        transform.LookAt(target);
+
+            transform.LookAt(target);
     }
+    
+    public void SlideCamera (bool left)
+    {
+        if (left)
+            // Rotate vector and slide left --else--swipe right
+            offset = Quaternion.Euler(0, 90, 0) * offset;
+        else
+            offset = Quaternion.Euler(0, -90, 0) * offset;
+    }
+    
 
 
 }
